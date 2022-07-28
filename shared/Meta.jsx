9eca12353b2d44ea2641d3ffb41ed85f58005453
Manolingam N <manolingam1996@gmail.com>
2022-07-28 14:49:30 +0530

    meta info

--- shared/Meta.jsx
+++ shared/Meta.jsx
@@ -5,9 +5,22 @@ export const Meta = () => {
     <>
       <Head>
         <title>Chakravyuha</title>
-        <meta name='description' content='' />
-        <meta property='og:title' content='Chakravyuha' />
-        <meta property='og:description' content='' />
+        <meta name='description' content='The moloch slayer of Raidguild.' />
+        <meta property='og:title' content='The Chakravyuha' />
+        <meta
+          property='og:description'
+          content='The moloch slayer of Raidguild.'
+        />
+        <meta property='og:image' content='/assets/logos/chakravyuga.webp' />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='The Chakravyuha' />
+        <meta
+          name='twitter:description'
+          content='The moloch slayer of Raidguild.'
+        />
+        <meta name='twitter:image' content='/assets/logos/chakravyuga.webp' />
+        <meta property='og:type' content='website' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
     </>
